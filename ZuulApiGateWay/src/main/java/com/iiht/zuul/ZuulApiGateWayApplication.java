87aef7672b63d10ef88b96455bf7fdea6bde8b91
javalaxman <91667702+javalaxman@users.noreply.github.com>
2022-03-09 13:40:41 +0530

    Add files via upload

---
+++ ZuulApiGateWay/src/main/java/com/iiht/zuul/ZuulApiGateWayApplication.java
@@ -0,0 +1,17 @@
+package com.iiht.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGateWayApplication.class, args);
+	}
+
+}
